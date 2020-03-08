4655cdca0afb2ad50229be05f74743814d0d57b9
theflyingmonkeyninja <dhananjayghangale@outlook.com>
2020-03-08 21:53:51 +0100

    added volatality models

--- volatilitymodels.py
+++ volatilitymodels.py
@@ -7,3 +7,40 @@ Created on Sat Mar  7 19:21:35 2020
 """
 
 # historical simulation, EWMA, GARCH, ARMA  
+
+import yfinance as yf  
+import numpy as np     
+import matplotlib.pyplot as plt
+
+def historicalvolatility(s):
+         
+    l = np.size(s,0);
+    ind = int(.01*l);
+    
+    ret = np.log10(s[1:]/s[:-1]); 
+    
+    ret = np.sort(ret);
+    
+    histrisk = ret[ind]
+    
+    
+    paramrisk = -2.33*np.std(ret);
+    
+    if paramrisk != histrisk:
+        print 'gaussian distribution of returns is not correct assumption'
+        
+        
+    
+    
+    return paramrisk, histrisk,ret;
+
+
+
+
+
+# Get the data for the stock Apple by specifying the stock ticker, start date, and end date
+st1 = yf.download('TSLA','2014-01-01','2020-01-01')
+s = st1.to_numpy(); s = s[:,2];
+p,h,r = historicalvolatility(s)
+
+plt.hist(r)
