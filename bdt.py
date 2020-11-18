d31ab60366d0c9b7fc3443c82d03d040008dfe0a
Dhananjay Ghangale <44656191+theflyingmonkeyninja@users.noreply.github.com>
2020-11-18 11:29:46 +0100

    Update bdt.py

--- bdt.py
+++ bdt.py
@@ -5,15 +5,14 @@ Created on Tue Nov 10 14:06:00 2020
 @author: dhana
 """
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+
 
 def BlackDermanToyInterestRate(ZCRates,ZCVol,ParVal):
     # this function computes the short rate for BDT model
     # MatPeriod is years to maturity of Zero coupon Bond
     # ZCRate is the zero coupon rate in %
     # ZCVol is the zero coupon Volatility in %
-    # FVal face value of ZC-Bond
+    # ParVal face value of ZC-Bond
     
     treelength = len(ZCRates); # length of the binomial tree
     
@@ -31,49 +30,81 @@ def BlackDermanToyInterestRate(ZCRates,ZCVol,ParVal):
      
     for i in range(1,treelength):
         
-        guess = 0.0787
-        bsum = 0
+            
+        x = np.zeros(1000);
+        x[0] = 0.0;
+        x[1] = 0.079;
         
-    
-        while bsum!=ZCBPrice[i]:
+        Sigma = np.exp(2*ZCVol[i]); 
+        
+        
+        for itr in range(2,2000):
             
-            Sigma = np.exp(2*ZCVol[i]); 
-            bsum = 0;
-            Dfact = 1
+            # fixing  the intreval for the root
             
-            if  i==1:
-                Dfact =1;
-            elif i==2:
-                Dfact = np.prod(SRate[0,:]+1);
-            else:
-                for k in range(i-1):
-                    Dfact = Dfact* np.prod(SRate[k,:]+1);
-            
+            F0 = np.zeros([treelength,treelength]);
+            F1 = np.zeros([treelength,treelength]);
+       
+          
             
             for j in range(treelength):
-                if j<i:
-                    bsum = bsum +(0.5**i*ParVal/(1+guess*Sigma**j)+0.5**i*ParVal/(1+guess*Sigma**(j+1)))/(Dfact*(1+SRate[i-1,j]))    
-                  
+                if j<=i:
+                   
+                    F0[i,j] = ParVal/(1+x[itr-2]*Sigma**(j))    
+                    F1[i,j] = ParVal/(1+x[itr-1]*Sigma**(j))
+                    
+
                 else:
                     break;
+             
+            for m in reversed(range(i)):
+                for n in range (i):
+                    if n<=m:
+                        F0[m,n] = (0.5*F0[m+1,n]+0.5*F0[m+1,n+1])/(1+SRate[m,n])
+                        F1[m,n] = (0.5*F1[m+1,n]+0.5*F1[m+1,n+1])/(1+SRate[m,n])
+              
+            f0 = F0[0,0]
+            f1 = F1[0,0]
+            
+            f0 = ZCBPrice[i]-f0;
+            f1 = ZCBPrice[i]-f1;
+                
+            
+            x[itr] = x[itr-1]-f1*(x[itr-1]-x[itr-2])/(f1-f0);
+            
+            F = np.zeros([treelength,treelength]);
+                        
+            for j in range(treelength):
+                 if j<=i:
+                                      
+                     F[i,j] = ParVal/(1+x[itr]*Sigma**(j));
+                   
                     
-                    
-            if np.absolute(bsum-ZCBPrice[i])<0.5:
-                 SRate[i,j] = guess;
+                 else:
+                     break;
+                     
+                     
+            for m in reversed(range(i)):
+                for n in range (i):
+                    if n<=m:
+                        F[m,n] = (0.5*F[m+1,n]+0.5*F[m+1,n+1])/(1+SRate[m,n])         
+                     
+            f = F[0,0];
+            f = ZCBPrice[i]-f;
+             
+                           
+            if np.abs(f)<0.000001:
+                 SRate[i,i] = x[itr]; 
                  break;
-            elif bsum<ZCBPrice[i]:
-                guess = (guess-.001);
-                if guess<0:
-                    print('error')
-            elif bsum>ZCBPrice[i]:
-                guess = (guess+0.001);
            
-        length = j+1;
+          
+        length = i+1;
+        
         Temp = np.zeros(length); 
-        m = j;
+        m = i;
         
         for k in range(length):            
-            Temp[k] = SRate[i,j]*Sigma**m;
+            Temp[k] = SRate[i,i]*Sigma**m;
             m = m-1
         
         Temp = np.flip(Temp);
@@ -84,14 +115,32 @@ def BlackDermanToyInterestRate(ZCRates,ZCVol,ParVal):
             else:
                 break;
                 
-    
-    return SRate;      
+        
+                
+    SRate = SRate*100
+    return SRate,F;      
 
 
 
-ZCRates = np.array( [9,9.5,10,10.5,11]);
+# ZCRates = np.array([10,11,12,12.5,13]);
+# ZCRates = ZCRates/100;
+# ZCVol = np.array([20,19,18,17,16]);
+# ZCVol = ZCVol/100;
+# ParVal = 100
+# SRate = BlackDermanToyInterestRate(ZCRates,ZCVol,ParVal);
+
+# ZCRates = np.array([9,9.5,10,10.5,11]);
+# ZCRates = ZCRates/100;
+# ZCVol = np.array([24,22,20,18,16]);
+# ZCVol = ZCVol/100;
+# ParVal = 100
+# (F,SRate) = BlackDermanToyInterestRate(ZCRates,ZCVol,ParVal);
+
+ZCRates = np.array([10,11,12,12.5,13]);
 ZCRates = ZCRates/100;
-ZCVol = np.array([24,22,20,18,16]);
+ZCVol = np.array([20,19,18,17,16]);
 ZCVol = ZCVol/100;
 ParVal = 100
-SRate = BlackDermanToyInterestRate(ZCRates,ZCVol,ParVal);
+(F,SRate) = BlackDermanToyInterestRate(ZCRates,ZCVol,ParVal);
+
+
